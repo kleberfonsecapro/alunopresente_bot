dcd70d187c7c9788bb808b5ebb35d74466aa70c3
kleber <kleber.souza@sme.cuiaba.mt.gov.br>
2026-08-19 18:23:29 -0300

    chore: remove app scraper_bot (renomeado para aluno_presente_sme)

--- backend/scraper_bot/skills/extractors/__init__.py
+++
@@ -1,9 +0,0 @@
-from scraper_bot.skills.extractors.base import SiteExtractor
-from scraper_bot.skills.extractors.registry import (
-    get_extractor,
-    list_site_choices,
-    list_site_types,
-    register,
-)
-from scraper_bot.skills.extractors.generic import GenericExtractor
-from scraper_bot.skills.extractors.aluno_presente import AlunoPresenteExtractor
